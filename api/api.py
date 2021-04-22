6590c665c1e579344fd38b97384002cac1001106
Athul <athulrthulasidasan@gmail.com>
2021-04-22 21:47:49 +0530

    solved link issues

--- api/api.py
+++ api/api.py
@@ -14,6 +14,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 
 df = pd.read_csv("./gifthub_train_revised_num.csv")
 df.head()
@@ -45,64 +46,124 @@ def scraper(site):
     soup = BeautifulSoup(r.text, "html.parser")
     lst = soup.findAll("div", {"class": "KZmu8e"})
     for div in lst:
-        anchor = re.findall("(<a[^>]*>)", str(div.a))
-        dct["link"] = anchor[0]
-        dct["img"] = div.find("img")["src"]
-        dct["title"] = div.find("div", {"class": "sh-np__product-title translate-content"}).text.strip()
-        dct["price"] = div.find("span", {"class": "T14wmb"}).text.strip()
-        anchor = dct["price"][1:].split()
-        # print(anchor[0])
-        dct["price_num"] = float(anchor[0].replace(",", "").split("₹")[0])
-        dct["seller"] = div.find("span", {"class": "E5ocAb"}).text.strip()
+        anchor = re.findall('(<a[^>]*>)', str(div.a))
+        ind = anchor[0].find('href')
+        link = anchor[0][ind+6:-2]
+        
+        ind = link.find("http")
+        link = link[ind:].replace("amp;","")
+    
+        if link.startswith("http://www.flipkart.com/"):
+            ind = link.find("%3Fpid")
+            dct["link"] = link[:ind]
+        else:
+            dct["link"] = link
+
+        if div.find('img') == None:
+            dct["img"] = "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png"
+        else:
+            dct["img"] = div.find('img')['src']
+        
+        dct["title"] = div.find('div', {"class": "sh-np__product-title translate-content"}).text.strip()
+        
+        price = div.find('span', {"class": "T14wmb"}).text.strip()
+        ind = price.find('₹',1)
+        if ind != -1:
+            dct["price"] = price[:ind]
+        else:
+            ind = price.find(".")
+            price = price[:ind+3] + " " + price[ind+3:]
+            dct["price"] = re.sub('\s+',' ',price)
+
+        ind = dct["price"].find('.')
+        dct["price_num"] = float((dct["price"][1:ind+3]).replace(',',''))
+        
+        dct["seller"] = div.find('span', {"class": "E5ocAb"}).text.strip()
+        
         dct_lst.append(dct.copy())
 
     lst = soup.findAll("div", {"class": ("u30d4")})
     lst = lst[:-1]
     for div in lst:
-        anchor = re.findall("(<a[^>]*>)", str(div.a))
-        dct["link"] = anchor[0]
-        dct["img"] = div.find("img")["src"]
-        anchor = div.div.next_sibling.div.a
-        dct["title"] = anchor.text.strip()
-        dct["price"] = div.find("span", {"class": "HRLxBb"}).text.strip()
-        anchor = dct["price"][1:].split()
-        # print(anchor[0])
-        dct["price_num"] = float(anchor[0].replace(",", ""))
-        anchor = div.find("div", {"class": "dD8iuc"}).text.strip()
-        elem = re.findall("(from.*)", anchor)
+        anchor = re.findall('(<a[^>]*>)', str(div.a))
+        ind = anchor[0].find('href')
+        link = anchor[0][ind+6:-2]
+
+        ind = link.find("http")
+        link = link[ind:].replace("amp;","")
+    
+        if link.startswith("http://www.flipkart.com/"):
+            ind = link.find("%3Fpid")
+            dct["link"] = link[:ind]
+        else:
+            dct["link"] = link
+        
+        if div.find('img') == None:
+            dct["img"] = "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png"
+        else:
+            dct["img"] = div.find('img')['src']
+
+        try:
+            anchor = div.div.next_sibling.div.a
+            dct["title"] = anchor.text.strip()
+        except:
+            dct["link"] = "None"
+            dct["img"] = "None"
+            dct["title"] = "None"
+            dct["price"] = "-1"
+            dct["price_num"] = "-1"
+            dct["seller"] = "-1"
+            continue
+
+        
+        price = div.find('span', {"class": "HRLxBb"}).text.strip()
+        ind = price.find('₹',1)
+        if ind != -1:
+            dct["price"] = price[:ind]
+        else:
+            ind = price.find(".")
+            price = price[:ind+3] + " " + price[ind+3:]
+            dct["price"] = re.sub('\s+',' ',price)
+
+        ind = dct["price"].find('.')
+        dct["price_num"] = float((dct["price"][1:ind+3]).replace(',',''))
+        
+        anchor = div.find('div', {"class": "dD8iuc"}).text.strip()
+        elem = re.findall('(from.*)', anchor)
         if elem:
             dct["seller"] = elem[0]
         else:
             dct["seller"] = " "
+        
         dct_lst.append(dct.copy())
-    res_lst = [i for n, i in enumerate(dct_lst) if i not in dct_lst[n + 1 :]]
+    res_lst = [i for n, i in enumerate(dct_lst) if i not in dct_lst[n + 1:]]
     return res_lst
 
 
 def gifthub(age, gender, relation, ocassion, interest1, interest2, budget):
     user_input = [[age, gender, relation, ocassion, interest1, interest2, budget]]
-    df = pd.DataFrame(
-        user_input, columns=["age", "gender", "relation", "occasion", "interest_1", "interest_2", "budget"]
-    )
+
+    df = pd.DataFrame(user_input, columns = ['age', 'gender', 'relation', 'occasion', 'interest_1', 'interest_2', 'budget'])
     output = dtree.predict(df)
     output = str(output[0])
     prediction = output
-    output = output.split(",")  # output list ready ex. ['Audio Sunglasses', ' Gaming Console']
-
+    output = output.split(',')  # output list ready ex. ['Audio Sunglasses', ' Gaming Console']
+    
     out_lst = []
     for data in output:
-        data = prepdata(data)  # prepping terms from output for scraping
+        data = prepdata(data) # prepping terms from output for scraping
         site = "https://www.google.com/search?q=" + data + "&source=lnms&tbm=shop&sa=X"
         lst = scraper(site)
-        out_lst = out_lst + lst
+        out_lst = out_lst + lst 
 
-    sor_out_lst = sorted(out_lst, key=lambda i: i["price_num"])  # sorting all the scraped items
+    sor_out_lst = sorted(out_lst, key = lambda i: i['price_num']) # sorting all the scraped items
+    
     if user_input[0][6] == 0:
-        return [prediction , sor_out_lst[: len(sor_out_lst) // 3]]
+        return [prediction , sor_out_lst[:len(sor_out_lst)//3] ]
     elif user_input[0][6] == 1:
-        return [prediction, sor_out_lst[len(sor_out_lst) // 3 : 2 * (len(sor_out_lst) // 3)]]
+        return [prediction , sor_out_lst[len(sor_out_lst)//3:2*(len(sor_out_lst)//3)]]
     else:
-        return [prediction , sor_out_lst[2 * (len(sor_out_lst) // 3) :]]
+        return [prediction , sor_out_lst[2*(len(sor_out_lst)//3):]]
 
 
 # ghout = gifthub([[1,0,0,0,0,0,0]])[0:2]
